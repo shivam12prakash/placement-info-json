6e26dd8f1f04b3b5a110bc77de193c99c2ff2705
Shivam Prakash <shivam12prakash@gmail.com>
2021-06-15 17:06:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import NavBar from './components/navbar';
+import LandingPage from './components/LandingPage';
+import Users from './components/Users';
+import AddUsers from './components/AddUsers';
+import EditUser from './components/EditUser';
+import NotFound from './components/NotFound';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={LandingPage} exact/>
+        <Route path="/all" component={Users} exact/>
+        <Route path="/add" component={AddUsers} exact/>
+        <Route path="/edit/:id" component={EditUser} exact/>
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
